7d74ef97be4d2c4a9639174146e0a94fc3375e0d
mehrdad.koolaji <mehrdad.koolaji>
2025-08-16 00:06:14 +0330

    solve some problems

--- base_web_app/outputs.tf
+++ base_web_app/outputs.tf
@@ -11,3 +11,8 @@ output "instance_dns" {
 output "aws_instance_public_dns" {
   value = "http://${aws_instance.nginx1.public_dns}"
 }
+
+
+output "aws_alb_public_dns" {
+  value = "http://${aws_lb.nginx.dns_name}"
+}
